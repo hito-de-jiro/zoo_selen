a830005dd636d64a37543fe731ac9614ac73500b
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2022-10-13 20:21:04 +0300

    First finally version 0.00.1

--- zoo_selen.py
+++ zoo_selen.py
@@ -5,67 +5,71 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 
 HOST = 'https://www.zooplus.de'
-num_page = 1
-url = f'https://www.zooplus.de/tierarzt/results?animal_99=true&page={str(num_page)}'
+URL = f'https://www.zooplus.de/tierarzt/results?animal_99=true'
 
 
-def get_html(url=url):
-    # start browser without options
-    browser = webdriver.Chrome()
-    # make dictionary  for list get_data
+def get_html(url=URL):
+    """Запускає браузер, отримує та повертає необроблені дані"""
+    # Створення списку для збіру необроблених даних
     all_data = []
+    num_page = 3    # Кількість сторінок для парсингу
+    i = 1
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(10)
+    while i <= num_page:
+        browser.get(url + '&page=' + str(i))
+        print('Parsing the page # %s' % i)
+        try:
+            WebDriverWait(browser, 3).until(ec.element_to_be_clickable((By.XPATH,
+                                                                        "/html/body/div[2]/div[3]/div/div/div[2]"
+                                                                        "/div/div/button[2]"))).click()
+        except TimeoutException:
+            pass
+        finally:
+            elems = browser.find_elements(By.CLASS_NAME, 'result-intro ')
+            #  get value from list of elems
+            for elem in elems:
+                # make empty list and record data elements
+                get_data = []
+                empty_string = 'empty value'
+                title = elem.find_element(By.CLASS_NAME, 'result-intro__title')
+                get_data.append(title.text)
+                rating = elem.find_element(By.CLASS_NAME, 'result-intro__rating__note')
+                get_data.append(rating.text)
+                try:
+                    description = elem.find_element(By.CLASS_NAME, 'result-intro__subtitle')
+                    get_data.append(description.text)
+                except NoSuchElementException:
+                    get_data.append(empty_string)
 
-    try:
-        browser.get(url)
-        # Operation with window cookies
-        WebDriverWait(browser, 20).until(
-            ec.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
-        time.sleep(1)
-        elems = browser.find_elements(By.CLASS_NAME, 'result-intro ')
-        # get value from list of elems
-        for elem in elems:
-            # make empty list and record data elements
-            get_data = []
-            empty_string = ''
-            title = elem.find_element(By.CLASS_NAME, 'result-intro__title')
-            get_data.append(title.text)
-            rating = elem.find_element(By.CLASS_NAME, 'result-intro__rating__note')
-            get_data.append(rating.text)
-            try:
-                description = elem.find_element(By.CLASS_NAME, 'result-intro__subtitle')
-                get_data.append(description.text)
-            except Exception:
-                get_data.append(empty_string)
+                try:
+                    working_time = elem.find_element(By.CLASS_NAME, 'daily-hours__range')
+                    get_data.append(working_time.text)
+                except NoSuchElementException:
+                    get_data.append(empty_string)
 
-            try:
-                working_time = elem.find_element(By.CLASS_NAME, 'daily-hours__range')
-                get_data.append(working_time.text)
-            except Exception:
-                get_data.append(empty_string)
+                try:
+                    working_time_note = elem.find_element(By.CLASS_NAME, 'daily-hours__note').text
+                    get_data.append(working_time_note)
+                except NoSuchElementException:
+                    get_data.append(empty_string)
 
-            try:
-                working_time_note = elem.find_element(By.CLASS_NAME, 'daily-hours__note').text
-                get_data.append(working_time_note.text)
-            except Exception:
-                get_data.append(empty_string)
+                address_text = elem.find_element(By.CLASS_NAME, 'result-intro__address').text
+                address = address_text.replace('\n', '')
+                get_data.append(address)
+                # record list 'get_data' in dict 'all_data'
+                all_data.append(get_data)
+        print(f'Parsing the page # {i} successfully completed.')
+        i += 1
 
-            address_text = elem.find_element(By.CLASS_NAME, 'result-intro__address').text
-            address = address_text.replace('\n', '')
-            get_data.append(address)
-            print(get_data)
-            # record list 'get_data' in dict 'all_data'
-            all_data.append(get_data)
-
-    except Exception as ex:
-        print(ex)
-    finally:
-        print('Done!')
-        time.sleep(5)
-        browser.close()
-        browser.quit()
-
+    print('Done!')
+    time.sleep(5)
+    browser.close()
+    browser.quit()
     return all_data
 
 
@@ -77,11 +81,11 @@ def save_csv(data):
     csv_file.close()
 
 
-def main():
+def main(url=URL):
     data = get_html(url)
     save_csv(data)
+    print('Welldone!')
 
 
 if __name__ == '__main__':
     main()
-
